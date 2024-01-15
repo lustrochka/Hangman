178ac38ac56e71ba1d7a58c01d2af941e4c92d55
Olga <kartoshkaslukom@gmail.com>
2024-01-15 18:04:30 +0100

    feat: add hangman

--- Hangman/src/scripts/gallows.js
+++ Hangman/src/scripts/gallows.js
@@ -1,45 +1,11 @@
-import GallowsSrc from '../assets/viselitsa.svg';
+import { image } from './image';
 
 export class Gallows {
   render() {
-    const gallowsBlock = document.createElement('div');
-    gallowsBlock.classList.add('gallows-block');
+    const gallows = document.createElement('div');
+    gallows.classList.add('gallows-block');
+    gallows.innerHTML = image;
 
-    const gallows = document.createElement('img');
-    gallows.src = GallowsSrc;
-    gallows.classList.add('gallows');
-    gallowsBlock.appendChild(gallows);
-
-    const hangman1 = document.createElement('div');
-    hangman1.innerText = '◯';
-    hangman1.classList.add('hangman');
-    gallowsBlock.appendChild(hangman1);
-
-    const hangman2 = document.createElement('div');
-    hangman2.innerText = '|';
-    hangman2.classList.add('hangman');
-    gallowsBlock.appendChild(hangman2);
-
-    const hangman3 = document.createElement('div');
-    hangman3.innerText = '/';
-    hangman3.classList.add('hangman');
-    gallowsBlock.appendChild(hangman3);
-
-    const hangman4 = document.createElement('div');
-    hangman4.innerText = '\\';
-    hangman4.classList.add('hangman');
-    gallowsBlock.appendChild(hangman4);
-
-    const hangman5 = document.createElement('div');
-    hangman5.innerText = '/';
-    hangman5.classList.add('hangman');
-    gallowsBlock.appendChild(hangman5);
-
-    const hangman6 = document.createElement('div');
-    hangman6.innerText = '\\';
-    hangman6.classList.add('hangman');
-    gallowsBlock.appendChild(hangman6);
-
-    return gallowsBlock;
+    return gallows;
   }
 }
